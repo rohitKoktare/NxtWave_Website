a3fbbc507927d9881903a6feb6d81d3f073d2a11
root <rohitkoktarebugatti@gmail.com>
2023-04-07 04:13:34 +0000

    thirdPart

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
 import Products from './components/Products'
 import Cart from './components/Cart'
 import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
 
 import './App.css'
 
@@ -12,10 +13,11 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={LoginForm} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
       <Route exact path="/cart" component={Cart} />
-      <Route component={NotFound} />
+      <Route path="/not-found" component={NotFound} />
+      <Redirect to="/not-found" />
     </Switch>
   </BrowserRouter>
 )
---
+++ src/components/ProtectedRoute/index.js
@@ -0,0 +1,14 @@
+import Cookies from 'js-cookie'
+import {Route, Redirect} from 'react-router-dom'
+
+const ProtectedRoute = props => {
+  const jwtToken = Cookies.get('jwtToken')
+  if (jwtToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Route {...props} />
+}
+
+export default ProtectedRoute
